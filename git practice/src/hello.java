a168f389f3125bb57253cf0067c9b5c253f2f132
RAAGA\sahitya <jubin.yadllapalli@gmail.com>
2017-09-17 10:24:08 -0500

    1st change

--- git practice/src/hello.java
+++ git practice/src/hello.java
@@ -3,7 +3,9 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("1st program");
+		System.out.println("1st program");
+		System.out.println("1st change");
+
 	}
 
 }
